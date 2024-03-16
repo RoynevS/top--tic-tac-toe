5c177451e4586dac22dd95a4afea11747adc684d
RoynevS <109765130+RoynevS@users.noreply.github.com>
2024-03-16 15:48:33 +0100

    Create gameboard
    creates a gameboard that can be printed and allows to change values of
    the board.

--- script.js
+++ script.js
@@ -5,3 +5,25 @@ function createPlayer(name, marker) {
   return { getName, getMarker };
 }
 
+
+function gameboard() {
+  const board = [
+    [" ", " ", " "],
+    [" ", " ", " "],
+    [" ", " ", " "],
+  ];
+
+  const getBoard = () => board;
+
+  const setMarks = (row, column, player) => {
+    if (board[row][column] === "X" || board[row][column] === "O") return;
+
+    board[row][column] = player.getMarker();
+  };
+
+  const printBoard = () => {
+    console.log(board);
+  };
+  
+  return { getBoard, setMarks, printBoard };
+}
