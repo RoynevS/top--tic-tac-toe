cdc1814c77209b9399b7b895ba36d3232c15ba20
RoynevS <109765130+RoynevS@users.noreply.github.com>
2024-03-17 14:45:46 +0100

    Add UI for the game and fix player switch bug
    before when clicking a filled field it changed the player. Now nothing
    happens until the player clicks a valid field or the game is over.

--- script.js
+++ script.js
@@ -15,8 +15,13 @@ const gameboard = (function() {
 
   const getBoard = () => board;
 
+  const checkCellEmpty = (row,column) => {
+    if (board[row][column] === "X" || board[row][column] === "O") return false;
+    return true;
+  }
+
   const setMarks = (row, column, player) => {
-    if (board[row][column] === "X" || board[row][column] === "O") return;
+    if (!checkCellEmpty(row, column)) return;
 
     board[row][column] = player.getMarker();
   };
@@ -25,7 +30,7 @@ const gameboard = (function() {
     console.table(board);
   };
   
-  return { getBoard, setMarks, printBoard };
+  return { getBoard, setMarks, printBoard, checkCellEmpty };
 })();
 
 const gameController = (function() {
@@ -57,9 +62,11 @@ const gameController = (function() {
   }
 
 
-  const playRound = () => {
-    const [row, column] = prompt(`${getActivePlayer().getName()} enter row and column to place your marker:`)
-      .split(" ");
+  const playRound = (row, column) => {
+    // const [row, column] = [null, null]
+    // // prompt(`${getActivePlayer().getName()} enter row and column to place your marker:`)
+    // //   .split(" ");
+    if (!board.checkCellEmpty(row, column)) return;
 
     console.log(
       `Placing ${getActivePlayer().getName()} marker into row ${row} and column ${column}...`
@@ -70,7 +77,7 @@ const gameController = (function() {
     if (!gameEnd()) {
       switchPlayerTurn();
     }
-    
+
     printNewRound();
 
   };
@@ -130,12 +137,52 @@ const gameController = (function() {
   const gameEnd = () => checkRow() || checkColumn() || checkDiagonal() || checkDraw();
 
   printNewRound();
-  
-  while (!gameEnd()) {
-    playRound();
-  }
 
-  return { playRound, getActivePlayer };
+  return { playRound, getActivePlayer, getBoard: board.getBoard };
 })();
 
-const game = gameController;
+
+function screenController() {
+  const game = gameController;
+  const playerActiveDiv = document.querySelector(".active-player");
+  const boardDiv = document.querySelector(".board");
+
+  const updateScreen = () => {
+    boardDiv.textContent = "";
+
+    const board = game.getBoard();
+    const activePlayer = game.getActivePlayer();
+    console.log(activePlayer.getName())
+
+    playerActiveDiv.textContent = `${activePlayer.getName()}'s turn...`;
+
+    let counter = 0;
+    board.forEach(row => {
+      row.forEach((cell, index) => {
+        const cellButton = document.createElement("button");
+        cellButton.classList.add("cell");
+        cellButton.dataset.column = index;
+        cellButton.dataset.row = counter;
+        cellButton.textContent = cell;
+        boardDiv.appendChild(cellButton);
+      })
+      counter++
+    })
+  };
+
+  function clickHandlerBoard(e) {
+    const selectedColumn = e.target.dataset.column;
+    const selectedRow = e.target.dataset.row;
+
+    if (!selectedColumn || !selectedRow) return;
+
+    game.playRound(selectedRow, selectedColumn);
+    updateScreen();
+  }
+
+  boardDiv.addEventListener("click", clickHandlerBoard);
+
+  updateScreen();
+}
+
+screenController();
